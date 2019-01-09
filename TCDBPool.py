23dd2b662b38bc012f2ef023ab18fc5f7204d9a1
Derek <derekj77@gmail.com>
2019-01-09 11:29:40 -0600

    formatting

--- TCDBPool.py
+++ TCDBPool.py
@@ -1,12 +1,13 @@
 from TCDBConnection import TCDBConnection
 from Queue import Queue
 
+
 class TCDBPool:
     def __init__(self):
         self.connections = {}
         self.free = Queue()
         self.cons = 0
-    
+
     def Connect(self, _host, _user, _password, _database, _connections):
         self.cons = _connections
         for i in range(_connections):
@@ -14,15 +15,15 @@ class TCDBPool:
             con.Connect(_host, _user, _password, _database)
             self.connections[i] = con
             self.free.put(i)
-    
+
     def GetConnection(self):
         i = self.free.get()
         return (i, self.connections[i])
-    
+
     def FreeConnection(self, i):
         self.free.put(i)
-    
-    def QueryOne(self, query, args = None):
+
+    def QueryOne(self, query, args=None):
         i = -1
         try:
             i, con = self.GetConnection()
@@ -30,8 +31,8 @@ class TCDBPool:
         finally:
             if i != -1:
                 self.FreeConnection(i)
-                
-    def QueryAll(self, query, args = None):
+
+    def QueryAll(self, query, args=None):
         i = -1
         try:
             i, con = self.GetConnection()
